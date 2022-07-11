61209991a57dd877529bf43bf1557f58dfa84a0c
georgediversity <80828296+GeorgeLoukovitis@users.noreply.github.com>
2022-07-11 14:22:17 +0300

    Smart Contract Incoperates Support

--- SmartContracts/SmartContract.sol
+++ SmartContracts/SmartContract.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+uint256 constant MAX_PREREQUISITES = 5;
+
 contract AssignmentContract{
 
-    // uint256[] public milestones;
+    struct Achievement {
+        uint256 milestone;
+        string data;
+        uint256[] support;
+    }
     
     
-    mapping(uint256 => mapping(address => bool)) public userMilestones;
+    mapping(address => mapping(uint256 => Achievement)) public userMilestones;
     mapping(uint256 => mapping(address => bool)) public milestoneAuthorities;
     mapping(uint256 => uint256[]) public milestonePrerequisites;
     
@@ -14,14 +20,13 @@ contract AssignmentContract{
     // Add milestone to milestones
     function createMilestone(uint256 _milestone) public 
     {
-        // milestones.push(_milestone);
         milestoneAuthorities[_milestone][msg.sender] = true;
     }
 
     // Check if an adress has a milestone in userMilestones
     function hasMilestone(address _usr, uint256 _milestone) public view returns(bool)
     {
-        return userMilestones[_milestone][_usr];
+        return (userMilestones[_usr][_milestone].milestone > 0);
     }
 
     function meetsPrerequisites(address _user, uint256 _milestone) public view returns(bool)
@@ -29,25 +34,26 @@ contract AssignmentContract{
         uint256[] memory _prerequisites = milestonePrerequisites[_milestone];
         for(uint i = 0; i<_prerequisites.length; i++)
         {
-            if(!userMilestones[_prerequisites[i]][_user])
+            if(!(userMilestones[_user][_prerequisites[i]].milestone > 0))
                 return false;
         }
         return true;
     }
 
     // Add a milestone to an adress in userMilestones
-    function awardMilestone(address _user, uint256 _milestone) public 
+    function awardMilestone(address _user, uint256 _milestone, string memory data) public 
     {
-        require(isAdmin(msg.sender, _milestone));
-        require(meetsPrerequisites(_user, _milestone));
-        userMilestones[_milestone][_user] = true;
+        require(isAdmin(msg.sender, _milestone), "You are not an authority of this milestone");
+        require(meetsPrerequisites(_user, _milestone), "The user doesn't meet the prerequisites of the Milestone");
+        uint256[] memory support;
+        userMilestones[_user][_milestone] = Achievement(_milestone, data, support);
     }
     
     // Check if milestone1 has milestone2 as a prerequisite in milestonePrerequisites
     function hasPrerequisite(uint256 _milestone1, uint256 _milestone2) public view returns(bool)
     {
         uint256[] memory _prerequisites = milestonePrerequisites[_milestone1];
-        for(uint i = 0; i<_prerequisites.length; i++)
+        for(uint i = 0; i<_prerequisites.length && i<MAX_PREREQUISITES; i++)
         {
             if(_milestone2 == _prerequisites[i])
                 return true;
@@ -58,7 +64,9 @@ contract AssignmentContract{
     // Add a milestone2 to milestone1's prerequisites in milestonePrerequisites
     function addMilestonePrerequisite(uint256 _milestone1, uint256 _milestone2) public
     {
-        require(isAdmin(msg.sender, _milestone1));
+        require(isAdmin(msg.sender, _milestone1), "Only a milestone authority can add a prerequisite to a contract");
+        require(milestonePrerequisites[_milestone1].length < MAX_PREREQUISITES, "There is a finite number of prerequisites a milestone can have");
+
         milestonePrerequisites[_milestone1].push(_milestone2);
     }
 
@@ -73,6 +81,19 @@ contract AssignmentContract{
     {
         require(isAdmin(msg.sender, _milestone));
         milestoneAuthorities[_milestone][_user] = true;
+    }
+
+    function checkSupportItem(address _user, uint256 _milestone, uint number) public view returns (uint256)
+    {
+        require(userMilestones[_user][_milestone].milestone > 0,  "The user hasn't been awarded that milestone");
+        require(number < userMilestones[_user][_milestone].support.length, "The index should be smaller than the size of the array");
+        return userMilestones[_user][_milestone].support[number];
+    }
+
+    function addSupportItem(address _user, uint256 _milestone, uint256 _supportMilestone) public
+    {
+        require(userMilestones[_user][_milestone].milestone > 0,  "The user hasn't been awarded that milestone");
+        userMilestones[_user][_milestone].support.push(_supportMilestone);
     }
 
 
